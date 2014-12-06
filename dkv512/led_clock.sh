349f00b5f5213262f72dff4684d54b4190897511
emptyland <cjvoid@gmail.com>
2014-12-07 01:11:17 +0800

    Update dkv512.

--- dkv512/led_clock.sh
+++ dkv512/led_clock.sh
@@ -2,15 +2,17 @@
 
 LEDS="25 26 27 28"
 
+CMD="/usr/local/bin/gpio"
+
 for led in $LEDS; do
-    gpio mode $led out
+    $CMD mode $led out
 done
 
 for ((i=0; i<7; i++)); do
     for led in $LEDS; do
-        gpio write $led 1
+        $CMD write $led 1
         sleep 0.2
-        gpio write $led 0
+        $CMD write $led 0
     done
 done
 
